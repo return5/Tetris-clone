899fcfd9156db2da89f7a5fafd3e1231e9079157
chris <44552743+return5@users.noreply.github.com>
2020-03-31 12:24:41 -0500

    Add files via upload

--- compiletetris.sh
+++ compiletetris.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 #compile script for tetris
-#compiles tetris and creates executable named 'tetris'
+#compiles tetris and creates executable named 'tetris_clone'
 #defaults to using gcc, but uses clang if user supplies '-c' or '--clang' as argument to script. 
 #use argumnet --help to bring up help info
 
 #prints the help message to terminal.
 printHelp() {
-	printf "compile script for tetris\n"
-	printf "compiles tetris.c and creates executable names 'tetris'\n"
-	printf "defaults to using gcc, but uses clang if user supplies '-c' to script.\n\n"
-	printf "options:\n"
-	printf " -c   --clang 	use clang instead of gcc\n"
-	printf "-help  --help 	prints this page then exits\n"
+    printf "%s\n" "compile script for teris_clone"
+    printf "%s\n" "compiles teris_clone and creates executable names 'teris_clone'"
+    printf "%s\n\n" "defaults to using gcc, but uses clang if user supplies '-c' to script."
+    printf "%s\n\t" "options:"
+    printf "%s\n\t" "-c   --clang   use clang instead of gcc"
+    printf "%s\n" "-help  --help   prints this page then exits"
 }
 
 #use GCC to compile the game
 useGCC() {
-	printf "using GCC\n"
-	gcc tetris.c -orogueclone -Wall -Wextra -O2 -finline-functions -Wswitch-enum -lncurses -std=gnu11 -otetris 
+    printf "%s\n" "using GCC"
+    gcc -orogueclone -Wall -Wextra -O2 -Iheaders -finline-functions -Wswitch-enum -lncurses -std=gnu11 -oteris_clone
 }
 
 #compiles game using Clang
 useClang() {
-	printf "using Clang\n"
-	clang tetris.c -Wall -Wextra -O2 -lncurses -finline-functions -Wswitch-enum -std=gnu11 -otetris   
+    printf "%s\n" "using Clang"
+    clang -Iheaders -Wall -Wextra -O2 -lncurses -finline-functions -Wswitch-enum -std=gnu11 -oteris_clone   
 }
 
 main() {
@@ -33,14 +33,13 @@ main() {
             ;;
         -c | --clang)           #if argument passed is the clang flag
             #check if clang is installed, if it is, call the useClang function, other wise fallback to GCC
-            (clang -v >/dev/null 2>&1 && useClang) || (printf "falling back to GCC.\n" && (gcc -v >/dev/null 2>&1 && useGCC) || printf "error, make sure GCC is installed.\n")   
+            (clang -v >/dev/null 2>&1 && useClang) || (printf "%s\n" "falling back to GCC." && (gcc -v >/dev/null 2>&1 && useGCC || printf "error, make sure GCC is installed.\n"))   
             ;;
         *)
             #checks to see if gcc is installed, and if so call useGCC, otherwise print error
-            (gcc -v >/dev/null 2>&1 && useGCC) || printf "error, make sure GCC is installed.\n"
+            (gcc -v >/dev/null 2>&1 && useGCC) || printf "%S\n" "error, make sure GCC is installed."
             ;;
     esac
 }
-
 #call main function and pass it positonal paramter 1
 main "$1"
